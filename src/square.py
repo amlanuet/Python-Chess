1ded86f6aa086fce3a7926ba4e83a99a24e1a9be
Teun Alma <teun.alma001@fclive.nl>
2023-03-21 14:02:22 +0100

    Added out of bounds static method
    added a method that can always be called without having an object attached to it so we can always check if a specific move falls outside of the board even tho we don't have any information about the outside of the board

--- src/square.py
+++ src/square.py
@@ -8,3 +8,11 @@ class Square:
 
     def has_piece(self):
         return self.piece != None
+    
+    @staticmethod
+    def in_range(*args):
+        for arg in args:
+            if arg < 0 or arg > 7:
+                return False
+        return True
+    
